34a1dcbbc76c63ec42a8a192c23482931703b727
Rhogoro Norine <rhogoron@ev-g3.kzn.21-school.ru>
2024-05-07 14:22:56 +0300

    WebSocketController

--- ChatService/src/main/java/org/example/controller/WebSocketController.java
+++ ChatService/src/main/java/org/example/controller/WebSocketController.java
@@ -1,6 +1,22 @@
 package org.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WebSocketController {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/send-message")
+    public void sendMessage(@Payload String message, SimpMessageHeaderAccessor accessor) {
+        String token = accessor.getFirstNativeHeader("Authorization");
+        messagingTemplate.convertAndSendToUser(accessor.getUser().getName(), "/topic/success", "successMessage"");
+    }
 
 
 
