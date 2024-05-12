77a6df67d3e930799c93f3a54e81648bc484d74b
Rhogoro Norine <rhogoron@ev-g3.kzn.21-school.ru>
2024-05-12 13:40:08 +0300

    new service socket

--- test/src/main/java/org/example/WebSocketMessageSender.java
+++ test/src/main/java/org/example/WebSocketMessageSender.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
@@ -9,26 +10,41 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.lang.reflect.Type;
 import java.util.concurrent.ExecutionException;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.util.Collections;
 
 public class WebSocketMessageSender {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
     public static void main(String[] args) throws Exception {
-        WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        WebSocketClient webSocketClient = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
         MyStompSessionHandler sessionHandler = new MyStompSessionHandler();
 
-        // Установка авторизационных данных, если это необходимо
-        // HttpHeaders headers = new HttpHeaders();
-        // headers.add("Authorization", "Bearer your_access_token");
+        // Создаем заголовки запроса
+        StompHeaders headers = new StompHeaders();
+        headers.add("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiRU1QTE9ZRUUiLCJzdWIiOiJyaG9nb3JvbiIsImlhdCI6MTcxNTAwNDYyMCwiZXhwIjoxNzE1MDkxMDIwfQ.pMlbIVJdUzhMftHURecsXjv0c6IdweuQiVKbGZV4yT4D-RV7gpwOskOfGzoTCh40au4nilcfrMBLmT0jQ1uP7g");
 
-        stompClient.connect("ws://localhost:8081/ws", sessionHandler);
+        String wsUrl = "ws://localhost:8081/ws";
+        stompClient.connect(wsUrl, sessionHandler, headers);
 
         Thread.sleep(5000); // Ждем несколько секунд, чтобы установить соединение
 
         StompSession session = sessionHandler.getSession();
-        session.send("/app/messages/senderId/recipientId/count", null);
+        session.send("/app/chat.createRoom/MyRoom", null);
+
         String chatMessage = "сообщение для чата";
         String jsonMessage = objectMapper.writeValueAsString(chatMessage);
-        session.send("/app/chat", jsonMessage.getBytes());
+        session.send("/app/chat.sendMessage/1", jsonMessage.getBytes());
     }
 
     private static class MyStompSessionHandler extends StompSessionHandlerAdapter {
