c12d71bdcf78e6fba3d1793dea60eee678561b2b
Rhogoro Norine <rhogoron@ev-g3.kzn.21-school.ru>
2024-05-13 12:43:12 +0300

    mongo connect

--- untitled/src/main/java/org/example/controller/WebSocketController.java
+++ untitled/src/main/java/org/example/controller/WebSocketController.java
@@ -1,26 +1,19 @@
 package org.example.controller;
 
-
 import org.example.exception.CustomException;
+import org.example.jwt.JwtTokenProvider;
 import org.example.model.Message;
 import org.example.model.Room;
 import org.example.model.dto.SenderDto;
-import org.example.security.jwt.JwtTokenProvider;
 import org.example.service.MessageService;
 import org.example.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.security.Principal;
 
 import static java.lang.System.out;
@@ -44,14 +37,15 @@ public class WebSocketController {
     public void createRoom( @org.springframework.messaging.handler.annotation.DestinationVariable String roomName, Principal principal) throws CustomException {
         out.println("roomName == " + roomName + ", principal == " + principal);
         if (principal instanceof Authentication) {
-//            Authentication authentication = (Authentication) principal;
-//            String jwtToken = (String) authentication.getCredentials();
-//            String senderName = jwtTokenProvider.getUsernameFromToken(jwtToken);
-//            Room room = new Room(roomName, senderName);
-//            roomService.saveRoom(room);
-//            messagingTemplate.convertAndSend("/topic/room/" + room.getId(), room);
+            Authentication authentication = (Authentication) principal;
+            if (authentication.getPrincipal() instanceof SenderDto) {
+                SenderDto senderDto = (SenderDto) authentication.getPrincipal();
+                String senderName = senderDto.getName();
+                Room room = new Room(roomName, senderName);
+//                roomService.saveRoom(room);
+                messagingTemplate.convertAndSend("/topic/room/" + room.getId(), room);
+            }
         }
-
         messagingTemplate.convertAndSend("/topic/", "error");
     }
 
@@ -66,15 +60,12 @@ public class WebSocketController {
                 String userName = senderDto.getName();
                 String userRole = senderDto.getRole();
                 out.println("userName == " + userName + " userRole == " + userRole);
+                Room room = roomService.findById(roomId);
+                out.println("room == " + room.getName());
+                Message messageUser = new Message(message, new SenderDto(userName, userRole), room);
 
-//            Authentication authentication = (Authentication) principal;
-//            String jwtToken = (String) authentication.getCredentials();
-//            String userName = jwtTokenProvider.getUsernameFromToken(jwtToken);
-//            String userRole = jwtTokenProvider.getRoleFromToken(jwtToken);
-//            Room room = roomService.findById(roomId);
-//            Message messageUser = new Message(message, new SenderDto(userName, userRole), room);
-//            messageService.save(messageUser);
-//            messagingTemplate.convertAndSend("/topic/room/" + roomId, messageUser);
+//                messageService.save(messageUser);
+                messagingTemplate.convertAndSend("/topic/room/" + roomId, messageUser);
             }
             messagingTemplate.convertAndSend("/topic/room/" + roomId, "error");
         }
---
+++ untitled/src/main/java/org/example/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,95 @@
+package org.example.jwt;
+
+
+import org.example.model.dto.SenderDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static java.lang.System.out;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter
+{
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            // Извлечение токена из запроса
+            String token = extractTokenFromRequest((HttpServletRequest) request);
+            out.println(token);
+            if (token != null) {
+                String role = jwtTokenProvider.getRoleFromToken(token);
+                out.println(role);
+                String userName = jwtTokenProvider.getUsernameFromToken(token);
+                out.println(userName);
+                SenderDto user = new SenderDto(userName, role);
+                if (user != null) {
+                    // Создание объекта аутентификации с ролями пользователя
+                    List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_" + user.getRole());
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                    // Установка объекта аутентификации в контекст безопасности
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    logger.info("User {} successfully authenticated with roles: {}" + user.getName() + " " + authorities);
+                }
+            }
+            // Продолжение цепочки фильтров
+            filterChain.doFilter(request, httpServletResponse);
+        } catch (Exception ex) {
+            messagingTemplate.convertAndSend("/topic/error", ex.getMessage());
+            // Обработка ошибок аутентификации
+            handleAuthenticationError((HttpServletResponse) httpServletResponse, ex);
+        }
+    }
+
+    // Метод для обработки ошибок аутентификации
+    private void handleAuthenticationError(HttpServletResponse response, Exception ex) throws IOException {
+        logger.error("Authentication error: " + ex.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        String errorMessage = "{\"result\": \"Failed\", \"message\": \"" + ex.getMessage() + "\", \"errorCode\": \"" + "401" + "\"}";
+        response.getWriter().write(errorMessage);
+    }
+
+    private String extractTokenFromRequest(HttpServletRequest request) {
+        String token = request.getParameter("token");
+        if (token == null) {
+            token = request.getHeader("Authorization");
+            if (token != null && token.startsWith("Bearer ")) {
+                token =  token.substring(7);
+            }
+        }
+        out.println("token == " + token);
+        if (token != null && !token.isEmpty()) {
+            return token;
+        }
+        return null;
+    }
+
+
+}
+
